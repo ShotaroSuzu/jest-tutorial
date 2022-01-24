c83a79f509ed61f77b257df645170110ae4c3565
ShotaroSuzu <cosmos.1220.3110@gmail.com>
2022-01-24 09:12:39 +0900

    async test add explanation

--- __tests__/async.test.ts
+++ __tests__/async.test.ts
@@ -1,6 +1,7 @@
 import { getRamenAfterOneSecond } from '@/async';
 
 test('Ramen Creator', () => {
+  // 非同期処理の場合は待たずに後続が実行されてしまうので、このテストは失敗する
   const ramen = getRamenAfterOneSecond();
   expect(ramen).toBe('sio ramen');
 });
