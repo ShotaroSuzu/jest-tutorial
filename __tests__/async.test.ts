affb749f963dc869e571bead578b037fa1d6dcd6
ShotaroSuzu <cosmos.1220.3110@gmail.com>
2022-01-24 09:47:56 +0900

    async fail

--- __tests__/async.test.ts
+++ __tests__/async.test.ts
@@ -1,7 +1,21 @@
 import { getRamenAfterOneSecond } from '@/async';
 
-test('Ramen Creator', () => {
-  // 非同期処理の場合は待たずに後続が実行されてしまうので、このテストは失敗する
-  const ramen = getRamenAfterOneSecond();
-  expect(ramen).toBe('sio ramen');
+describe('Ramen Creator', () => {
+  test('when order is sio, create sio ramen', () => {
+    // 非同期処理の場合は待たずに後続が実行されてしまうので、このテストは失敗する
+    //   const ramen = getRamenAfterOneSecond();
+    //   expect(ramen).toBe('sio ramen');
+    // テストでも通常の非同期処理を待つ場合と同じように、以下のように書く。
+    // しかし、 return は省略してかけない
+    return getRamenAfterOneSecond('sio').then((ramen) => {
+      expect(ramen).toBe('sio ramen');
+    });
+  });
+
+  test('when order is tonkotsu, return error', () => {
+    expect.assertions(1);
+    return getRamenAfterOneSecond('tonkotsu').catch((e) =>
+      expect(e.message).toMatch('うちは塩しかやってねーんだよ！')
+    );
+  });
 });
--- src/async.ts
+++ src/async.ts
@@ -1,13 +1,21 @@
-export const getRamenAfterOneSecond = (): string => {
-  let ramen = '';
-  setTimeout((ramen) => {
-    console.log('Getting order, creating ramen...');
-    ramen = createRamen(1000);
-  }, 10);
-  return ramen;
+export const getRamenAfterOneSecond = (order: string): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      try {
+        console.log('Getting order, creating ramen...');
+        resolve(createRamen(order, 1000));
+      } catch (e) {
+        reject(e);
+      }
+    }, 10);
+  });
 };
 
-const createRamen = (timeout: number) => {
+const createRamen = (menu: string, timeout: number) => {
+  if (menu !== 'sio') {
+    throw new Error('うちは塩しかやってねーんだよ！');
+  }
+
   console.log('start create ramen!');
   const startTime = Date.now();
   while (true) {
