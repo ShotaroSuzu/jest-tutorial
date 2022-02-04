3d4c0e21e0c8c189dc4c0ab88ccd8957246af665
ShotaroSuzu <cosmos.1220.3110@gmail.com>
2022-02-04 22:53:30 +0900

    mock test

--- __tests__/mock.test.ts
+++ __tests__/mock.test.ts
@@ -1,7 +1,8 @@
 import { myForEach, getUsers } from '@/mock';
 import axios from 'axios';
 import Users from '../src/users';
-import { mocked } from 'ts-jest/utils';
+import { mocked } from 'jest-mock';
+import { target } from '@/mockTarget';
 describe('mock properties', () => {
   test('mock properties', () => {
     //モック関数の作成とモックを渡す
@@ -74,4 +75,34 @@ test('should fetch users', () => {
   const resp = { data: users };
   mocked(axios, true).get.mockResolvedValue(resp);
   return Users.all().then((data) => expect(data).toEqual(users));
+});
+
+jest.mock('../src/mockTarget');
+describe('mock implementation', () => {
+  const mockedTarget = mocked(target);
+  describe('jest.fn mock implementation', () => {
+    it('implement as written by jest.fn', () => {
+      const err = 'Error';
+      const val = 2;
+      const myMockFn = jest.fn((cb) => cb(null, true));
+      myMockFn((_err: any, val: any) => console.log(val));
+    });
+  });
+  describe('mockImplementation', () => {
+    it('always return same value', () => {
+      mockedTarget.mockImplementation(() => 'mocked value');
+      console.log('mockedTarget is mocked by mockImplementation');
+      expect(mockedTarget()).toEqual('mocked value'); // 本当は検査対象のモジュールが依存しているモジュールをモックするのだが面倒くさいので省略
+    });
+  });
+  describe('呼び出すごとに異なる値が得られるケース', () => {
+    it('return different value in each call', () => {
+      mockedTarget.mockImplementationOnce(() => 'mocked value once');
+      mockedTarget.mockImplementationOnce(() => 'mocked value second');
+      console.log('mockedTarget is mocked by mockImplementationOnce');
+
+      expect(mockedTarget()).toEqual('mocked value once'); // 本当は検査対象のモジュールが依存しているモジュールをモックするのだが面倒くさいので省略
+      expect(mockedTarget()).toEqual('mocked value second');
+    });
+  });
 });
--- src/mockTarget.ts
+++ src/mockTarget.ts
@@ -1,1 +1,3 @@
-export const targetFn = () => {};
+export const target = (): string => {
+  return 'target implementation is called';
+};
